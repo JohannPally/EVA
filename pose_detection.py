368367f8da39ebfd6958dc4e55c39682feae0552
Johann Pally <pallyjohann@gmail.com>
2022-09-23 16:18:58 -0500

    added in some videos for testing. detection works, now have to test plotting

--- pose_detection.py
+++ pose_detection.py
@@ -1,7 +1,9 @@
 import cv2
 import mediapipe as mp
 import time
-import csv
+import matplotlib.pyplot as plt
+import mediapipe.python.solutions.drawing_utils
+from mpl_toolkits.mplot3d import Axes3D
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -10,12 +12,14 @@ mp_pose = mp.solutions.pose
 accum_results = []
 times = []
 
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('correct.MOV')
+
 with mp_pose.Pose(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as pose:
 
-    timeout = time.time() + 15
+    timeout = time.time() + 40
     while cap.isOpened() and (time.time() < timeout):
         success, image = cap.read()
         if not success:
@@ -28,12 +32,11 @@ with mp_pose.Pose(
         image.flags.writeable = False
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = pose.process(image)
-        # temp = []
-        # for lm in results.pose_landmarks.landmark:
-        #     temp.append(str(lm))
-        rslt_out = results.pose_landmarks.landmark
-        times.append(time.time())
-        accum_results.append(rslt_out)
+        # mediapipe.python.solutions.drawing_utils.plot_landmarks(results.pose_landmarks)
+
+        if results.pose_landmarks is not None:
+            times.append(time.time())
+            accum_results.append(results.pose_landmarks.landmark)
 
         # Draw the pose annotation on the image.
         image.flags.writeable = True
@@ -45,17 +48,20 @@ with mp_pose.Pose(
             landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
         # Flip the image horizontally for a selfie-view display.
 
-        cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+        cv2.imshow('MediaPipe Pose', cv2.flip(image,0))
         if cv2.waitKey(5) & 0xFF == 27:
           break
 
 cap.release()
+cv2.destroyAllWindows()
+
+
 # dump_json = json.dumps(accum_results, indent = 4)
-with open("test_data.csv", "w") as outfile:
-    writer = csv.writer(outfile)
-    for i in range(len(times)):
-        out = []
-        out.append(str(times[i]))
-        for lm in accum_results[i]:
-            out.append(str(lm))
-        writer.writerow(out)
+# with open("test_data.csv", "w") as outfile:
+#     writer = csv.writer(outfile)
+#     for i in range(len(times)):
+#         out = []
+#         out.append(str(times[i]))
+#         for lm in accum_results[i]:
+#             out.append(str(lm))
+#         writer.writerow(out)
