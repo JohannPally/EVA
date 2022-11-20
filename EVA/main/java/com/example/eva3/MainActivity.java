c437824708734ec14cb78f3997d916c41f1b55fa
Yuxuan Liu <liu686@wisc.edu>
2022-11-20 17:39:30 -0600

    Combined IMU reading and image processing
    Combined the code for both IMU and image, storing and returning the combined data into a operatable buffer

--- EVA/main/java/com/example/eva3/MainActivity.java
+++ EVA/main/java/com/example/eva3/MainActivity.java
@@ -23,14 +23,19 @@ import android.view.Surface;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketException;
 import java.nio.ByteBuffer;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class MainActivity extends AppCompatActivity implements ImageReader.OnImageAvailableListener {
 
@@ -43,13 +48,21 @@ public class MainActivity extends AppCompatActivity implements ImageReader.OnIma
 
         Button initSocket = findViewById(R.id.button_start_socket);
         View.OnClickListener startSocketListener = (v) -> {
-            initSocket();
+            try {
+                initSocket();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         };
         initSocket.setOnClickListener(startSocketListener);
 
         Button closeSocket = findViewById(R.id.button_close_socket);
         View.OnClickListener closeSocketListener = (v) -> {
-            closeSocket();
+            try {
+                closeSocket();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         };
         closeSocket.setOnClickListener(closeSocketListener);
 
@@ -70,93 +83,78 @@ public class MainActivity extends AppCompatActivity implements ImageReader.OnIma
     }
 
     boolean isTransmitting = false;
-    public void initSocket() {
+    boolean started = false;
+    Bitmap rgbFrameBitmap;
+    public void initSocket() throws IOException{
         System.out.println("init socket!!!");
-        Runnable processingTask =
-            () -> {
-                try {
-                    isTransmitting = true;
-                    socket = new Socket();
-                    socket.connect(new InetSocketAddress("192.168.1.76", 6650), 3000);
-                    socket.setSendBufferSize(2000);
-                    while(isTransmitting) {
-                        if(isBitMapReady) {
-                            try {
-                                ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                                rgbFrameBitmap.compress(Bitmap.CompressFormat.JPEG, 50, bos);
-                                byte[] array = bos.toByteArray();
-                                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-                                //System.out.println("Image Length: " + array.length);
-                                dataOutputStream.writeInt(array.length);
-                                dataOutputStream.write(array, 0, array.length);
-                                //System.out.println("!!!!!");
-                                imgCount += 1;
-                                //System.out.println("Image Count: " + imgCount);
-                                //dataOutputStream.flush();
-                                //System.out.println("###" + (System.nanoTime() - prev));
-                                isBitMapReady = false;
-                                //System.out.println("Image Count: 2");
-                                //dataOutputStream.close();
-                                //System.out.println("Image Count: 3");
-                            } catch (Exception e) {
-                                System.out.println("EXCEPTION!!!!!");
-                                e.printStackTrace();
-                                initSocket();
-                            }
-                        }
-                    }
-                    System.out.println("reached!!!");
-                    DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-                    dataOutputStream.writeInt(30);
-                    socket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            };
-
-        Runnable receivePromptTask =
-                () -> {
-                    MediaPlayer mp1 = MediaPlayer.create(this, R.raw.left_up);
-                    MediaPlayer mp2 = MediaPlayer.create(this, R.raw.right_up);
-                    MediaPlayer mp3 = MediaPlayer.create(this, R.raw.foward);
-                    MediaPlayer mp4 = MediaPlayer.create(this, R.raw.backward);
-                    mp1.setVolume(20, 20);
-                    mp2.setVolume(20, 20);
-                    mp3.setVolume(20, 20);
-                    mp4.setVolume(20, 20);
-
-                    while(isTransmitting) {
-                        if(socket != null && socket.isConnected()) {
-                            System.out.println(")))))");
-                            try {
-                                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-                                System.out.println("????");
-                                int result = Integer.parseInt(dataInputStream.readUTF());
-                                System.out.println(result + "!!!!");
-
-                                if (result == 1) {
-                                    mp1.start();
-                                } else if (result == 2) {
-                                    mp2.start();
-                                } else if (result == 3) {
-                                    mp3.start();
-                                } else {
-                                    mp4.start();
-                                }
-
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                };
-        new Thread(processingTask).start();
-        new Thread(receivePromptTask).start();
+        socket = new Socket();
+        socket.connect(new InetSocketAddress("172.20.10.2", 6650), 3000);
+        isTransmitting = true;
+        if(!started) {
+            System.out.println("Start task!!!");
+            new Thread(receivePromptTask).start();
+        }
+        started = true;
+        System.out.println("finished init socket!!!");
     }
 
-    public void closeSocket() {
+    Runnable receivePromptTask =
+        () -> {
+            MediaPlayer mp1 = MediaPlayer.create(this, R.raw.left_up);
+            MediaPlayer mp2 = MediaPlayer.create(this, R.raw.right_up);
+            MediaPlayer mp3 = MediaPlayer.create(this, R.raw.foward);
+            MediaPlayer mp4 = MediaPlayer.create(this, R.raw.backward);
+            mp1.setVolume(20, 20);
+            mp2.setVolume(20, 20);
+            mp3.setVolume(20, 20);
+            mp4.setVolume(20, 20);
+            try {
+                InputStream dataInputStream = socket.getInputStream();
+
+                while(isTransmitting) {
+                    if(socket != null && socket.isConnected()) {
+                        int result = 0;
+                        try {
+                            if(dataInputStream.available() != 0) {
+                                result = dataInputStream.read();
+                            }
+                        } catch(Exception e) {
+                            e.printStackTrace();
+                        }
+                        if (result == 1) {
+                            mp1.start();
+                        } else if (result == 2) {
+                            mp2.start();
+                        } else if (result == 3) {
+                            mp3.start();
+                        } else if(result == 0){
+                            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                            rgbFrameBitmap.compress(Bitmap.CompressFormat.JPEG, 50, bos);
+
+                            byte[] img_bytes = bos.toByteArray();
+                            byte[] size_bytes = ByteBuffer.allocate(4).putInt(img_bytes.length).array();
+
+                            byte[] buffer = new byte[size_bytes.length + img_bytes.length];
+                            System.arraycopy(size_bytes, 0, buffer, 0, size_bytes.length);
+                            System.arraycopy(img_bytes, 0, buffer, size_bytes.length, img_bytes.length);
+                            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                            dataOutputStream.write(buffer, 0, buffer.length);
+                            dataOutputStream.flush();
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        };
+
+
+
+    public void closeSocket() throws IOException {
         System.out.println("close socket!!!");
         isTransmitting = false;
+        started = false;
+        socket.close();
     }
 
     @Override
@@ -250,17 +248,11 @@ public class MainActivity extends AppCompatActivity implements ImageReader.OnIma
 
         } catch (final Exception ignored) {}
     }
-    private int imgCount = 0;
-    Bitmap rgbFrameBitmap;
-    boolean isBitMapReady = false;
-    long prev = System.nanoTime();
+
     private void processImage() {
-        isBitMapReady = false;
         imageConverter.run();
         rgbFrameBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Bitmap.Config.ARGB_8888);
         rgbFrameBitmap.setPixels(rgbBytes, 0, previewWidth, 0, 0, previewWidth, previewHeight);
-        isBitMapReady = true;
-        //ImageUtils.saveBitmap(rgbFrameBitmap, System.currentTimeMillis() + ".png");
         postInferenceCallback.run();
     }
 
